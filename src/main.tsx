2dcc6b72232cec538ec395f0eb42d738d19756de
KevinUrsett <kevinursett@gmail.com>
2026-08-03 09:25:53 +0200

    Use local mode in preview deployments

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 import { DriveLoginControl } from './components/DriveLoginControl';
+import PreviewRootApp from './PreviewRootApp';
 import RootApp from './RootApp';
+import { isLocalPreviewMode } from './lib/runtimeMode';
 import './fonts.css';
 import './styles.css';
 
 registerSW({ immediate: true });
 
+const previewMode = isLocalPreviewMode();
+const AppRoot = previewMode ? PreviewRootApp : RootApp;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RootApp />
-    <DriveLoginControl />
+    <AppRoot />
+    {!previewMode && <DriveLoginControl />}
   </StrictMode>
 );
